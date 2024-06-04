3ea2c518317231f4bf7689ad4c3fcba0b5b1cf23
Ricardo Guerra <RikkuG491@gmail.com>
2024-06-04 17:16:50 -0400

    Created content and starter code for Practice - Running JavaScript on Start

---
+++ index.js
@@ -0,0 +1,23 @@
+const firstMovie = {
+    title: "The Giant Gila Monster",
+    runtime: "108 minutes",
+    showtime: "04:00PM",
+    description: "A giant lizard terrorizes a rural Texas community and a heroic teenager attempts to destroy the creature.",
+    poster: "https://www.gstatic.com/tv/thumb/v22vodart/2157/p2157_v_v8_ab.jpg"
+};
+
+const movieTitles = ["The Giant Gila Monster", "Manos: The Hands Of Fate", "Time Chasers", "The Touch Of Satan", "Santa Claus Conquers The Martians", "Track Of The Moon Beast", "The Skydivers", "The Killer Shrews", "Project Moon Base", "The Giant Spider Invasion", "Catalina Caper", "Secret Agent Super Dragon", "Wild Rebels", "Danger: Diabolik", "Village Of The Giants"];
+
+function updateDOMElements(){
+    // Your code goes here
+}
+
+function displayMovieDetails(){
+    // Your code goes here
+}
+
+function addMovieTitleToList(movieTitle){
+    // Your code goes here
+}
+
+// Your code goes here
